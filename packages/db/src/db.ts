c8c00332f7dd5136c805bf56d3e30541099fa6d5
Mudia Geo <mudiageo@gmail.com>
2026-09-11 07:01:04 +0000

    Fix Cloudflare runtime crash: Remove dotenv import from db.ts

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema/index";
-import 'dotenv/config';
 
 const dbUrl = process.env.DATABASE_URL;
 
